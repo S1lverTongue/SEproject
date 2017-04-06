cb93f16dfe09f018f8e47811f9aefbc4c0777879
Masta Ambassada <sanders0492@users.noreply.github.com>
2017-04-06 11:39:59 -0500

    Added support for tag in PIPEntity

--- PIP/src/com/PIPEntity.java
+++ PIP/src/com/PIPEntity.java
@@ -5,13 +5,26 @@ public class PIPEntity {
 	 * Interface block:
 	 * 	void setID()
 	 * 	int getID()
+	 *
+	 * 	void setTag()
+	 * 	String getTag()
 	 */
 	private int id;
+	private String tag;
+
 	public void setID(int id) {
 		this.id = id;
 	}
 	
 	public int getID() {
 		return this.id;
+	}
+
+	public void setTag(String tag) {
+		this.tag = tag;
+	}
+
+	public String getTag() {
+		return this.tag;
 	}
 }
