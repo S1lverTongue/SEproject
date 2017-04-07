25c9bde739fe113d94306d6fc81a07f7d17da60a
S1lverTongue <tmay2013@hotmail.com>
2017-04-06 23:50:08 -0500

    Added PIPStart.java

--- PIP/src/app/PIPStart.java
+++ PIP/src/app/PIPStart.java
@@ -1,9 +1,28 @@
 package app;
 
 import java.nio.file.Files;
+import java.util.*;
+import java.io.*;
 
 public class PIPStart {
 	
-	
+	public static void main(String[] args) {
+		File directory = new File(".");
+		File[] files = directory.listFiles();
+		FileFilter fileFilter = new FileFilter() {
+			public boolean accept(File file) {
+				return file.isDirectory();
+			}
+		};
+		
+		File newDirectory = new File("./src/users/");
+		files = directory.listFiles(fileFilter);
+		System.out.println(directory.toString());
+		System.out.println(Arrays.toString(files));
+		System.out.println(directory.getAbsolutePath());
+		System.out.println(newDirectory.toString());
+		System.out.println(newDirectory.isDirectory());
+		System.out.println(newDirectory.getAbsolutePath());
+	}
 
 }
