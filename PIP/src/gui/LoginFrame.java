e4e61ddc669efd36df519fe2c838845b25897fb1
S1lverTongue <tmay2013@hotmail.com>
2017-04-06 23:51:59 -0500

    Playing with Login and PIPIO

--- PIP/src/gui/LoginFrame.java
+++ PIP/src/gui/LoginFrame.java
@@ -8,12 +8,15 @@ import java.awt.BorderLayout;
 import javax.swing.GroupLayout;
 import javax.swing.GroupLayout.Alignment;
 import javax.swing.LayoutStyle.ComponentPlacement;
+import javax.swing.ListModel;
 import javax.swing.ListSelectionModel;
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import javax.swing.JScrollPane;
 import javax.swing.JList;
+import javax.swing.DefaultListModel;
+import javax.swing.border.BevelBorder;
 
 public class LoginFrame extends JFrame {
 	private boolean goodLogin = false;
@@ -27,66 +30,61 @@ public class LoginFrame extends JFrame {
 		
 		JPanel panel = new JPanel();
 		getContentPane().add(panel, BorderLayout.EAST);
-		
-		JScrollPane scrollPane = new JScrollPane();
+		panel.setSize(panel.getParent().getWidth(), panel.getParent().getHeight());
 		
 		JButton btnNewButton = new JButton("Login User");
-		
 		JButton btnNewButton_1 = new JButton("Create User");
-		btnNewButton_1.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
-			}
-		});
+		
+		JList list = new JList();
+		DefaultListModel dlm = new DefaultListModel();
+		for (int i = 0; i < PIPIO.loadUsernames().size(); i++) {
+			dlm.add(i, PIPIO.loadUsernames().get(i));
+		}
+		list.setModel(dlm);
+		list.setVisible(true);
+		
+		JScrollPane scrollPane = new JScrollPane(list);
 		GroupLayout gl_panel = new GroupLayout(panel);
 		gl_panel.setHorizontalGroup(
-			gl_panel.createParallelGroup(Alignment.LEADING)
-				.addGroup(Alignment.TRAILING, gl_panel.createSequentialGroup()
+			gl_panel.createParallelGroup(Alignment.TRAILING)
+				.addGroup(gl_panel.createSequentialGroup()
 					.addContainerGap()
-					.addGroup(gl_panel.createParallelGroup(Alignment.TRAILING)
-						.addComponent(scrollPane, Alignment.LEADING, GroupLayout.DEFAULT_SIZE, 437, Short.MAX_VALUE)
+					.addGroup(gl_panel.createParallelGroup(Alignment.LEADING)
 						.addGroup(gl_panel.createSequentialGroup()
 							.addComponent(btnNewButton, GroupLayout.PREFERRED_SIZE, 220, GroupLayout.PREFERRED_SIZE)
 							.addGap(10)
-							.addComponent(btnNewButton_1, GroupLayout.DEFAULT_SIZE, 207, Short.MAX_VALUE)))
+							.addComponent(btnNewButton_1, GroupLayout.DEFAULT_SIZE, 207, Short.MAX_VALUE))
+						.addComponent(scrollPane, GroupLayout.PREFERRED_SIZE, 434, GroupLayout.PREFERRED_SIZE))
 					.addContainerGap())
 		);
 		gl_panel.setVerticalGroup(
-			gl_panel.createParallelGroup(Alignment.LEADING)
-				.addGroup(Alignment.TRAILING, gl_panel.createSequentialGroup()
+			gl_panel.createParallelGroup(Alignment.TRAILING)
+				.addGroup(gl_panel.createSequentialGroup()
 					.addContainerGap()
-					.addComponent(scrollPane, GroupLayout.PREFERRED_SIZE, 108, GroupLayout.PREFERRED_SIZE)
-					.addPreferredGap(ComponentPlacement.RELATED, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+					.addComponent(scrollPane, GroupLayout.PREFERRED_SIZE, 98, GroupLayout.PREFERRED_SIZE)
+					.addPreferredGap(ComponentPlacement.RELATED, 10, Short.MAX_VALUE)
 					.addGroup(gl_panel.createParallelGroup(Alignment.BASELINE)
 						.addComponent(btnNewButton, GroupLayout.PREFERRED_SIZE, 27, GroupLayout.PREFERRED_SIZE)
 						.addComponent(btnNewButton_1, GroupLayout.PREFERRED_SIZE, 29, GroupLayout.PREFERRED_SIZE))
 					.addContainerGap())
 		);
 		
-		JList list = new UserListing();
-		scrollPane.add(list);
+		
 		panel.setLayout(gl_panel);
+		
+		btnNewButton_1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				for (int i = 0; i < PIPIO.loadUsernames().size(); i++)
+				System.out.println(list.getModel().getElementAt(i));
+			}
+		});
 		setVisible(true);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
 	
-	class UserListing extends JList {
-		
-		public UserListing() {
-			String[] usernames = new String[PIPIO.loadUsernames().size()];
-			for (int i = 0; i < PIPIO.loadUsernames().size(); i++) {
-				usernames[i] = PIPIO.loadUsernames().get(i);
-			}
-			System.out.println(PIPIO.loadUsernames().size());
-			System.out.println(Arrays.toString(usernames));
-			setSelectionMode(ListSelectionModel.SINGLE_INTERVAL_SELECTION);
-		}
-	}
-	
-	/*
 	public static void main(String[] args) {
 		LoginFrame x = new LoginFrame();
 		x.setVisible(true);
 		x.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
-	*/
 }
