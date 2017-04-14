f16bdc4234e5860fa1161347f1686df1ab843268
S1lverTongue <tmay2013@hotmail.com>
2017-04-13 22:36:18 -0500

    Added a Description Shortener to Filter

--- PIP/src/util/Filter.java
+++ PIP/src/util/Filter.java
@@ -3,6 +3,9 @@ package util;
 public class Filter {
 	
 	private static String goodAscii = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789_";
+	
+	// Looks for any characters not specified in
+	// the goodAscii variable above
 	public static boolean userFilter(String data) {
 		boolean good = true;
 		int i = 0;
@@ -16,5 +19,17 @@ public class Filter {
 			i++;
 		}
 		return good;
+	}
+	
+	// Grabs a description or any string really and shortens
+	// it and adds an ellipsis on to it
+	public static String shortenDescription(String description) {
+		String returner = "";
+		if (description.length() >= 15) { // long string
+			returner = description.substring(0, 14).trim() + "...";
+		} else {
+			returner = description.trim();
+		}
+		return returner;
 	}
 }
